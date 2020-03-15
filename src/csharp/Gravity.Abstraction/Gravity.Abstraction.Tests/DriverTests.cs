05e293eacaed41fb8f85605cd40b314a770c746f
Gravity API <gravity.api@outlook.com>
2020-03-15 21:18:37 +0200

    add integration tests support for CI/CD

---
+++ src/csharp/Gravity.Abstraction/Gravity.Abstraction.Tests/DriverTests.cs
@@ -0,0 +1,32 @@
+﻿using Gravity.Abstraction.WebDriver;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gravity.Abstraction.Tests
+{
+    [TestClass]
+    public class DriverTests
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void CreateRemoteChrome()
+        {
+            // setup
+            var driverParams = "" +
+                "{" +
+                "    'driver':'RemoteWebDriver'," +
+                "    'remoteDriver':'ChromeDriver'," +
+                "    'driverBinaries':'" + TestContext.Properties["gridEndpoint"] + "'" +
+                "}";
+
+            // execute
+            var driver = new DriverFactory(driverParams).Create();
+
+            // assertion
+            Assert.IsTrue(driver != null);
+
+            // cleanup
+            driver.Dispose();
+        }
+    }
+}
